1b6d750658135a58005d88f3a84ac8487e07fac8
trysoehardi <trysuhardi5@gmail.com>
2023-11-20 22:46:57 +0700

    Add test and change name from express to regular name

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+const config = {
+  "roots" : [
+    "<rootDir>/test"
+  ],
+  "testMatch" : [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform" : {
+    "^.+\\.(ts|tsx)$" : "ts-jest"
+  },
+};
+
+export default config;
